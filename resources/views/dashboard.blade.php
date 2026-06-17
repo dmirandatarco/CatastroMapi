0130017c0b9a26bca7fc18a787babad68532f299
David Miranda <dmirandatarco@gmail.com>
2026-06-17 10:48:01 -0500

    final

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
         <div>
-            <h4 class="mb-3 mb-md-0">Bienvenidos al Sistema Catastral de </h4>
+            <h4 class="mb-3 mb-md-0">Bienvenidos al Sistema Catastral de Machupicchu</h4>
         </div>
     </div>
 
